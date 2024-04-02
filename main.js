c8f48b121735a7c40494be2bb955e40c2d6865ac
plants vs zombies <106076221+bob3ponja@users.noreply.github.com>
2024-04-02 07:48:58 -0300

    ..

---
+++ main.js
@@ -0,0 +1,10 @@
+const botoes = document .querySelectorAll(".botoes");
+
+for (let i = i < botoes.length; j++;){
+    botoes[i].onclick = function(){
+        for(let j = 0; j < botoes.length; j++){
+            botoes[j].classList.remove("ativo")
+    }
+botoes[i].classList.add("ativo");
+}
+}
